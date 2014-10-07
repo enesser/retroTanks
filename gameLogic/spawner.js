7073a76f686b1dfe2cc3a65c4aa3f7058096bb7c
Eric J Nesser <enesser@gmail.com>
2014-10-07 03:15:26 -0500

    New CSS dialog box, removed jQuery and BootBox

--- gameLogic/spawner.js
+++ gameLogic/spawner.js
@@ -3,7 +3,6 @@
 /* global Meteor: true */
 /* global Session: true */
 /* global _: true */
-/* global bootbox: true */
 var gs = Meteor.gameSpace = Meteor.gameSpace || {};
 
 // possible respawn colors using Atari's color pallete
@@ -27,21 +26,21 @@ var respawnColors = [{
  * @param  {number} preferredSpawnIndex
  * @param  {function} callback
  */
-function attemptSpawn(myTank, preferredSpawnIndex, callback) {
-	var spawnPoints = gs.scene.spawnPoints;
-	var spawnIndex = preferredSpawnIndex;
-	var spawnPoint = spawnPoints[preferredSpawnIndex];
+ function attemptSpawn(myTank, preferredSpawnIndex, callback) {
+ 	var spawnPoints = gs.scene.spawnPoints;
+ 	var spawnIndex = preferredSpawnIndex;
+ 	var spawnPoint = spawnPoints[preferredSpawnIndex];
 
-	var spawnInterval = Meteor.setInterval(function () {
-		var allTanks = gs.tankService.getAll();
-		var goodSpawnPoint;
+ 	var spawnInterval = Meteor.setInterval(function () {
+ 		var allTanks = gs.tankService.getAll();
+ 		var goodSpawnPoint;
 
-		if (!spawnPoint) {
-			spawnIndex = 0;
-			spawnPoint = spawnPoints[spawnIndex];
-		}
+ 		if (!spawnPoint) {
+ 			spawnIndex = 0;
+ 			spawnPoint = spawnPoints[spawnIndex];
+ 		}
 
-		goodSpawnPoint = true;
+ 		goodSpawnPoint = true;
 
 		//make sure tank doesn't spawn on top of an existing tank in the scene
 		for (var i = 0; i < allTanks.length; i++) {
@@ -61,40 +60,64 @@ function attemptSpawn(myTank, preferredSpawnIndex, callback) {
 			spawnPoint = spawnPoints[++spawnIndex];
 		}
 	}, 200);
-}
+ }
 
-gs.spawner = {
+ gs.spawner = {
 
 	/**
 	 * Respawn counter
 	 * @type {Number}
 	 */
-	respawnCount: 0,
+	 respawnCount: 0,
 
 	/**
 	 * Spawn tank into the scene
 	 */
-	spawnTank: function () {
-		var self = this;
-		var myTank = gs.tankService.getMyTank();
-		if (!myTank) {
-			bootbox.setDefaults({
-				closeButton: false,
-				animate: true
-			});
-			myTank = new gs.Tank();
+	 spawnTank: function () {
+	 	var self = this;
+	 	var myTank = gs.tankService.getMyTank();
+	 	if (!myTank) {
 
-			bootbox.prompt('What is your name?', function (result) {
-				if (result) {
-					if (result.length > 10) {
-						result = result.substr(0, 10) + '...';
-					}
-					myTank.playerName = result;
-					Session.set('sessionId', Meteor.uuid());
-					myTank.userId = Session.get('sessionId');
+	 		myTank = new gs.Tank();
 
-					var allTanks = gs.tankService.getAll();
-					var tankCount = allTanks.length;
+	 		//name dialog box
+	 		var input = document.querySelector('.inputPlayerName');
+	 		var focus = 50;
+
+	 		var focusBug = Meteor.setInterval(function() {
+	 			input.focus();
+
+	 			//TODO: figure this ridiculous bug out later
+	 			if (--focus <= 0)
+	 			{
+	 				Meteor.clearInterval(focusBug);
+	 			}
+	 		}, 10);
+
+	 		var button = document.querySelector('.inputPlayerName--button');
+	 		button.addEventListener('click', function() {
+
+	 			var result = input.value;
+
+	 			var overlay = document.querySelector('.enterPlayerName');
+	 			var dialog = document.querySelector('.enterPlayerName--dialog');
+
+	 			//hide name dialog box after submit
+	 			overlay.classList.add('hidden');
+	 			dialog.classList.add('hidden');
+
+	 			//truncate name if longer than 10 characters
+	 			if (result) {
+	 				if (result.length > 10) {
+	 					result = result.substr(0, 10) + '...';
+	 				}
+
+	 				myTank.playerName = result;
+	 				Session.set('sessionId', Meteor.uuid());
+	 				myTank.userId = Session.get('sessionId');
+
+	 				var allTanks = gs.tankService.getAll();
+	 				var tankCount = allTanks.length;
 
 					//find unused color
 					_.every(respawnColors, function (color) {
@@ -120,36 +143,37 @@ gs.spawner = {
 				} else {
 					self.spawnTank();
 				}
-			});
-		}
-	},
+
+			});	 			
+}
+},
 
 	/**
 	 * Attach controls
 	 */
-	attachControls: function () {
-		Meteor.setTimeout(function () {
-			window.addEventListener('keyup', gs.controls.keyUpHandler, false);
-			window.addEventListener('keydown', gs.controls.keyPressHandler, false);
-		}, 250);
-	},
+	 attachControls: function () {
+	 	Meteor.setTimeout(function () {
+	 		window.addEventListener('keyup', gs.controls.keyUpHandler, false);
+	 		window.addEventListener('keydown', gs.controls.keyPressHandler, false);
+	 	}, 250);
+	 },
 
 	/**
 	 * Respawn a tank after it's been damaged
 	 */
-	respawnTank: function (tank) {
-		var self = this;
-		var spawnPoints = gs.scene.spawnPoints;
-		++self.respawnCount;
-		attemptSpawn(tank, Math.floor(Math.random() * spawnPoints.length),
-			function (spawnPoint) {
-				gs.tankService.update(tank, {
-					x: spawnPoint.x,
-					y: spawnPoint.y,
-					angle: spawnPoint.angle,
-					damageTime: null
-				});
-				--self.respawnCount;
-			});
-	}
-};
+	 respawnTank: function (tank) {
+	 	var self = this;
+	 	var spawnPoints = gs.scene.spawnPoints;
+	 	++self.respawnCount;
+	 	attemptSpawn(tank, Math.floor(Math.random() * spawnPoints.length),
+	 		function (spawnPoint) {
+	 			gs.tankService.update(tank, {
+	 				x: spawnPoint.x,
+	 				y: spawnPoint.y,
+	 				angle: spawnPoint.angle,
+	 				damageTime: null
+	 			});
+	 			--self.respawnCount;
+	 		});
+	 }
+	};
